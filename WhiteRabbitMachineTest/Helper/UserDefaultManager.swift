26e218db95e27277af0039b4e6b9721a63596c55
Midhun KM <midhun@wacmob.com>
2022-06-19 22:13:22 +0530

    new update

---
+++ WhiteRabbitMachineTest/Helper/UserDefaultManager.swift
@@ -0,0 +1,18 @@
+//
+//  UserDefaultManager.swift
+//  WhiteRabbitMachineTest
+//
+//  Created by Midhun KM on 19/06/22.
+//
+
+import Foundation
+
+class UserDefaultManager {
+    
+    static let shared = UserDefaultsManager()
+    
+    
+    func getCustormerData() -> CustomerDetails {
+        
+    }
+}
